f524c3905b3f3b549b616e0fb9ee8eb7962ef7ae
mosubiyi-prog <mosubiyi@gmail.com>
2026-09-06 09:27:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-
+import 'places_service.dart';
 void main() {
   runApp(const DedaApp());
 }
