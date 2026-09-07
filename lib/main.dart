69ba0422e02517d784aa2b7c7cf40b6245951e3d
mosubiyi-prog <mosubiyi@gmail.com>
2026-09-07 15:52:22 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:latlong2/latlong.dart' hide Path;
 
 import 'places_service.dart';
 
